0f5edd2b5814ddc4eef5d0e044271b8a7fae83b4
CitInpt <90224615+CitInpt@users.noreply.github.com>
2022-05-04 16:33:26 +0000

    Update Python input 1.py

--- Python input 1.py
+++ Python input 1.py
@@ -18,3 +18,31 @@ input example :
 5
 1 2 3 4 5
 """
+
+
+
+
+###Here are some examples to master Python input:
+
+#get the number of test cases (integer)
+t = int(input())
+
+for i in range(t):
+	#first line contains the length of the permutation (integer)
+	n = int(input())
+	#we might not necessarily need this variable but it is crucial to maintain the structure of the input file
+	#in each test case we have two inputs so we need to call input() function twice in each iteration of this for loop
+	
+
+	#second line contains the permutation (integer list)
+	permutation = map(int, input().split()) #The split() method splits a string into a list. You can specify the separator, default separator is any whitespace (example : "1 2 3 4 5".split() = ['1','2','3','4','5'])
+	#the map(int ,list ) function cast each element of the list to an int (list[0]->int(list[0]) ...) 
+
+#now that the input is done we can do some processing on the input to get the result of the program
+
+
+
+### We recommend checking the official Python3 documentation [https://docs.python.org/3/](https://docs.python.org/3/) to fully understand the context of split(), map() functions
+
+
+
